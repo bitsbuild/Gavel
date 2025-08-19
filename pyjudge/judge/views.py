4c6ee418d2d82cdf3c62193e8ebdd4b43008a8c5
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-20 05:13:29 +0530

    PERMISSIONS COMPLETED

--- pyjudge/judge/views.py
+++ pyjudge/judge/views.py
@@ -9,18 +9,24 @@ from judge.serializers import (LanguageSerializer,
                                TestCaseSerializer,
                                SubmissionSerializer)
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
 class LanguageViewSet(ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    permission_classes = [IsAdminUser]
 class SetViewSet(ModelViewSet):
     queryset = Set.objects.all()
     serializer_class = SetSerializer
+    permission_classes = [IsAdminUser]
 class ChallengeViewSet(ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
+    permission_classes = [IsAdminUser]
 class TestCaseViewSet(ModelViewSet):
     queryset = TestCase.objects.all()
     serializer_class = TestCaseSerializer
+    permission_classes = [IsAdminUser]
 class SubmissionViewSet(ModelViewSet):
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
+    permission_classes = [IsAuthenticated]
