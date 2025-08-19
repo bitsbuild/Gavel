6d150c0a7e771e2db8bb63664c5aadbdda89a89e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-19 20:58:46 +0530

    Display Names Set For Completed Models

--- pyjudge/judge/models.py
+++ pyjudge/judge/models.py
@@ -5,11 +5,15 @@ class Language(Model):
     name = CharField(null=False,editable=True,primary_key=False)
     created = DateTimeField(auto_now_add=True,null=False,editable=False,primary_key=False)
     updated = DateTimeField(auto_now=True,null=False,editable=False,primary_key=False)
+    def __str__(self):
+        return self.name
 class Set(Model):
     id = UUIDField(default=uuid4,null=False,editable=False,primary_key=True)
     name = CharField(null=False,editable=True,primary_key=False)
     created = DateTimeField(auto_now_add=True,null=False,editable=False,primary_key=False)
-    updated = DateTimeField(auto_now=True,null=False,editable=False,primary_key=False)    
+    updated = DateTimeField(auto_now=True,null=False,editable=False,primary_key=False)
+    def __str__(self):
+        return self.name    
 class Challenge(Model):
     pass
 class TestCase(Model):
