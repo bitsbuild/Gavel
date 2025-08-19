eb606a99f26850debbbcde6d097b8fb8f624648c
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-19 20:12:13 +0530

    User App URLs Written

--- pyjudge/user/urls.py
+++ pyjudge/user/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from user.views import create,delete
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
-    
+    path("create/",create,name="create"),
+    path("get-token/",obtain_auth_token,name="get-token"),
+    path("delete/",delete,name="delete")
 ]
