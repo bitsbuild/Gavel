96b7bef51c949f93d1c2e3afc18a2c7b751aab20
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-20 00:33:40 +0530

    Judge App URLs Completed

--- pyjudge/judge/urls.py
+++ pyjudge/judge/urls.py
@@ -1,3 +1,16 @@
-urlpatterns = [
-    
-]
+from rest_framework.routers import DefaultRouter
+from judge.views import (
+    LanguageViewSet,
+    SetViewSet,
+    ChallengeViewSet,
+    TestCaseViewSet,
+    SubmissionViewSet
+)
+router = DefaultRouter()
+router.register("language",LanguageViewSet,basename="language")
+router.register("set",SetViewSet,basename="set")
+router.register("challenge",ChallengeViewSet,basename="challenge")
+router.register("testcase",TestCaseViewSet,basename="testcase")
+router.register("submission",SubmissionViewSet,basename="submission")
+urlpatterns = router.urls
+
