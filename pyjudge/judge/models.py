b1a2dabc0869ce1c49bb94ca12d81e56bede381a
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-19 21:32:32 +0530

    Challenge Model Completed

--- pyjudge/judge/models.py
+++ pyjudge/judge/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model,UUIDField,CharField,DateTimeField
+from django.db.models import Model,UUIDField,CharField,DateTimeField,TextField
 from uuid import uuid4
 class Language(Model):
     id = UUIDField(default=uuid4,null=False,editable=False,primary_key=True)
@@ -15,7 +15,16 @@ class Set(Model):
     def __str__(self):
         return self.name    
 class Challenge(Model):
-    pass
+    id = UUIDField(default=uuid4,null=False,editable=False,primary_key=True)
+    name = CharField(null=False,editable=True,primary_key=False)
+    challenge_body = TextField(null=False,editable=True,primary_key=False)
+    constraints = TextField(null=False,editable=True,primary_key=False)
+    input_instruction = TextField(null=False,editable=True,primary_key=False)
+    output_instruction = TextField(null=False,editable=True,primary_key=False)
+    created = DateTimeField(auto_now_add=True,null=False,editable=False,primary_key=False)
+    updated = DateTimeField(auto_now=True,null=False,editable=False,primary_key=False)
+    def __str__(self):
+        return self.name
 class TestCase(Model):
     pass
 class Submission(Model):
