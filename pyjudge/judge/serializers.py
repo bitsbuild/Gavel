d5def7456d6e5dfbe8812e8cdc3c5f71510a7198
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-08-20 05:11:16 +0530

    Endpoints Working Find, Next Up Is Processing Part Of Backend, Most Probably SERIALIZERS COMPLETED

--- pyjudge/judge/serializers.py
+++ pyjudge/judge/serializers.py
@@ -3,7 +3,7 @@ from judge.models import (Language,
                           Challenge,
                           TestCase,
                           Submission)
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SlugRelatedField,StringRelatedField
 class LanguageSerializer(ModelSerializer):
     class Meta:
         model = Language
@@ -13,14 +13,24 @@ class SetSerializer(ModelSerializer):
         model = Set
         fields = '__all__'
 class ChallengeSerializer(ModelSerializer):
+    challenge_set = SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field = 'name'
+    )
     class Meta:
         model = Challenge
         fields = '__all__'
 class TestCaseSerializer(ModelSerializer):
+    language = StringRelatedField()
+    challenge = StringRelatedField()
     class Meta:
         model = TestCase
         fields = '__all__'
 class SubmissionSerializer(ModelSerializer):
+    language = StringRelatedField()
+    challenge = StringRelatedField()
+    user = StringRelatedField()
     class Meta:
         model = Submission
         fields = '__all__'
