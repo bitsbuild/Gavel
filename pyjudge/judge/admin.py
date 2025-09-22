2b19a924a069c823dbc394e62a8b4c914323fc11
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 14:24:06 +0530

    -----RESTART-----

--- pyjudge/judge/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin import site
-from judge.models import Language,Set,Challenge,TestCase,Submission
-site.register(Language)
-site.register(Set)
-site.register(Challenge)
-site.register(TestCase)
-site.register(Submission)
--- pyjudge/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from user.views import create,delete
-from rest_framework.authtoken.views import obtain_auth_token
-urlpatterns = [
-    path("create/",create,name="create"),
-    path("get-token/",obtain_auth_token,name="get-token"),
-    path("delete/",delete,name="delete")
-]
