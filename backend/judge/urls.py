aa397f7ad9190f696a453de6a8f57a46759637dd
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 20:50:47 +0530

    BACKEND: Languages Endpoint Completed

--- backend/judge/urls.py
+++ backend/judge/urls.py
@@ -1,1 +1,5 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from judge.views import LanguageViewSet
+router = DefaultRouter()
+router.register(r'languages',LanguageViewSet,basename='languages')
+urlpatterns = router.urls
