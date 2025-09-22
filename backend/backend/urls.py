91d06518282bf7ac45bedfeb7f6817c8d220ee96
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 19:42:32 +0530

    BACKEND: Judge Backend Connected To Project Backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/',include('user.urls'))
+    path('api/user/',include('user.urls')),
+    path('api/judge/',include('judge.urls'))
 ]
