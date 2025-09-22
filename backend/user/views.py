31cb9daebefd802986c154096ffbb54ed5ea9793
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 17:31:53 +0530

    BACKEND: Delete User Endpoint Completed

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,19 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED,HTTP_200_OK,HTTP_400_BAD_REQUEST
 @api_view(['POST'])
-def create_user():
+def create_user(request):
     pass
 @api_view(['DELETE'])
-def delete_user():
-    pass
+def delete_user(request):
+    try:
+        request.user.delete()
+        return Response(
+            'Account Deleted Successfully',
+            status=HTTP_200_OK
+        )
+    except Exception as e:
+        return Response(
+            'Account Deletion Failed',
+            status=HTTP_400_BAD_REQUEST
+        )
