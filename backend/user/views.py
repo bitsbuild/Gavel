983183832b1d9f5beeb702c071db02c06c957c98
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 15:29:12 +0530

    BACKEND: Basic Views Defined

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,1 +1,7 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+@api_view(['POST'])
+def create_user():
+    pass
+@api_view(['DELETE'])
+def delete_user():
+    pass
