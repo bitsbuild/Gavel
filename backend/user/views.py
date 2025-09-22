7474b7f1349b1e687a520cb110da8247d005af25
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 17:35:28 +0530

    BACKEND: Create User Endpoint Written

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,9 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED,HTTP_200_OK,HTTP_400_BAD_REQUEST
+from user.serializer import UserSerializer
+
 @api_view(['POST'])
 def create_user(request):
-    pass
+    try:
+        serializer = UserSerializer(request.data)
+        serializer.validate(raise_exception=True)
+        serializer.save()
+        return Response(
+            'Account Created Successfully',
+            status=HTTP_201_CREATED
+        )
+    except Exception as e:
+        return Response(
+            {
+                'Status':'Account Deletion Failed',
+                'Error':str(e)
+            },
+            status=HTTP_400_BAD_REQUEST
+        )
 @api_view(['DELETE'])
 def delete_user(request):
     try:
@@ -14,6 +31,9 @@ def delete_user(request):
         )
     except Exception as e:
         return Response(
-            'Account Deletion Failed',
+            {
+                'Status':'Account Deletion Failed',
+                'Error':str(e)
+            },
             status=HTTP_400_BAD_REQUEST
         )
