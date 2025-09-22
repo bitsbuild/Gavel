a8c78c7f965b72c3165fb1a0bd618ae6cfbd09f7
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 22:11:58 +0530

    BACKEND: Basic Seriazlizers For All Models Completed

--- backend/judge/serializer.py
+++ backend/judge/serializer.py
@@ -1,6 +1,37 @@
 from rest_framework.serializers import ModelSerializer
-from judge.models import Language
+from judge.models import (
+    Language,
+    Problem,
+    ProblemSet,
+    ProblemInputOutput,
+    ProblemSetMapping,
+    Status,
+    Submission)
 class LangaugeSerializer(ModelSerializer):
     class Meta:
         model = Language
+        fields = '__all__'
+class ProblemSerializer(ModelSerializer):
+    class Meta:
+        model = Problem
+        fields = '__all__'
+class ProblemSetSerializer(ModelSerializer):
+    class Meta:
+        model = ProblemSet
+        fields = '__all__'
+class ProblemInputOutputSerialzier(ModelSerializer):
+    class Meta:
+        model = ProblemInputOutput
+        fields = '__all__'
+class ProblemSetMappingSerializer(ModelSerializer):
+    class Meta:
+        model = ProblemSetMapping
+        fields = '__all__'
+class StatusSerializer(ModelSerializer):
+    class Meta:
+        model = Status
+        fields = '__all__'
+class SubmissionSerializer(ModelSerializer):
+    class Meta:
+        model = Submission
         fields = '__all__'
