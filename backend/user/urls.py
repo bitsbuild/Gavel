244c95edd5e904692d6f35fab7c92641b58e8116
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 15:32:37 +0530

    BACKEND: URLs Written For User App

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from user.views import create_user,delete_user
+from rest_framework.authtoken.views import obtain_auth_token
+urlpatterns = [
+    path('create_user/',create_user,name='create_user'),
+    path('get_token/',obtain_auth_token,name='get_token'),
+    path('delete_user/',create_user,name='delete_user')
+]
