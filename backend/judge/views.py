72d76537ed73c9fcabd152dd771989e2bd1e4424
Jash Upadhyay <jashupadhyay.java@gmail.com>
2025-09-22 20:45:37 +0530

    BACKEND: Language ViewSet Written

--- backend/judge/views.py
+++ backend/judge/views.py
@@ -1,1 +1,6 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from judge.models import Language
+from judge.serializer import LangaugeSerializer
+class LanguageViewSet(ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LangaugeSerializer
